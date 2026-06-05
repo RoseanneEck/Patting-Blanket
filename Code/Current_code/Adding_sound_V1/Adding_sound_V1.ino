b6a4f1f70ee2761c188b52d707f3ef0b3c242c42
Roseanne <roseanne.eckheart@gmail.com>
2026-06-05 17:41:23 +0100

    Add ramdom fuction to tracks

--- Code/Current_code/Adding_sound_V1/Adding_sound_V1.ino
+++ Code/Current_code/Adding_sound_V1/Adding_sound_V1.ino
@@ -20,14 +20,14 @@ unsigned servoUpPos = 30;     // number to write to servo for it to be up.
 
 int PatThresh = 90;  // when sensor is triggered
 int PatHyst = 20;    // a sort of debouce
-bool held = false;
-//int track = random(1,5);  // picks a number from 1 to 5 (for when I get more tracks)
+bool held = false;   // boolean based on weather a person if continuing to hold the sensor
+
 int track = 1;
-held = false;  // boolean based on weather a person if continuing to hold the sensor 
+
 
 // ---------------
-// Servo movement states 
-// --------------- 
+// Servo movement states
+// ---------------
 
 
 enum State {
@@ -47,22 +47,21 @@ void setup() {
 
   Serial.begin(9600);
   softwareSerial.begin(9600);
- 
 
- 
-  if (player.begin(softwareSerial)) { // Start communication with DFPlayer Mini (this plays the MP3 tracks)
 
-    Serial.println("OK"); // DFPlayer mini will print ok in the serial monitor if no issues 
+
+  if (player.begin(softwareSerial)) {  // Start communication with DFPlayer Mini (this plays the MP3 tracks)
+
+    Serial.println("OK");  // DFPlayer mini will print ok in the serial monitor if no issues
 
     // Set volume to maximum (0 to 30).
     player.volume(20);
   } else {
-    Serial.println("Connecting to DFPlayer Mini failed!"); 
+    Serial.println("Connecting to DFPlayer Mini failed!");
   }
 
-  patterServo.attach(9); // pin the servo is attached to.
-  patterServo.write(servoUpPos); // make sure servo starts in the starting up position. 
-  
+  patterServo.attach(9);          // pin the servo is attached to.
+  patterServo.write(servoUpPos);  // make sure servo starts in the starting up position.
 }
 
 void loop() {
@@ -72,10 +71,10 @@ void loop() {
   unsigned long now2 = millis();
 
   // ----------------
-  // INPUT HANDLING 
+  // INPUT HANDLING
   // ----------
 
-  bool triggerPat = false; //Always set false in the loop so if it's set true at the next bit of logic it's only true for 1 frame.
+  bool triggerPat = false;  //Always set false in the loop so if it's set true at the next bit of logic it's only true for 1 frame.
 
   // Detect NEW trigger only if not already held
   if (!held && sensorVal > (PatThresh + PatHyst)) {
@@ -88,62 +87,54 @@ void loop() {
   // Detect release
   if (held && sensorVal < (PatThresh - PatHyst)) {
     held = false;
-Serial.println("RELEASE HELD");
+    Serial.println("RELEASE HELD");
   }
 
-  // ----------- 
-  // Triggering MP3 when a Pat is sensed 
+  // -----------
+  // Triggering MP3 when a Pat is sensed
   // --------------
 
-  if (triggerPat) && (now2 - lastChangeMusic > track_length)) {   
-      lastChangeMusic = now2;
-      player.play(track);  //(track) is the interger we named before - currently there is only one, but can be more.
-    }   // If pat is sensed (but its not just being held) and its not already playing then please play track. 
-
-// ----------------------------------------
+  if ((triggerPat) && (now2 - lastChangeMusic > track_length)) {
+    int track = random(1,4);  // picks a number from 1 to 4 - so it will play a ramdom track. 
+    player.play(track);  //(track) is the interger we named before - currently there is only one, but can be more.
+      // If pat is sensed (but its not just being held) and its not already playing then please play track.
+lastChangeMusic = now2;
+  }                  
+  // ----------------------------------------
   // SERVO STATE MACHINE
   // ----------------------------------------
 
+  switch (state) {
+    case IDLE:
+      patterServo.write(servoUpPos);
 
-switch (state) {
-  case IDLE:
-    patterServo.write(servoUpPos);
-   // Serial.println("  IDLE"); // Can uncomment these for debug - will show what state it is in
-   // Serial.println(sensorVal); // Can uncomment these for debug - will show sensor value 
-    if (triggerPat) {  // Move to PAT_DOWN state if pat is sensed (but not if someone is just holding sensor.)
-      state = PAT_DOWN;
-      lastChange = now;
+      if (triggerPat) {  // Move to PAT_DOWN state if pat is sensed (but not if someone is just holding sensor.)
+        state = PAT_DOWN;
+        lastChange = now;
+      }
+      break;
 
-    }
-    break;
+    case PAT_DOWN:
+      patterServo.write(servoDownPos);  // in PAT_DOWN state, move servo down to pat
 
-  case PAT_DOWN:
-    patterServo.write(servoDownPos);  // in PAT_DOWN state, move servo down to pat
-   // Serial.println("  PAT_DOWN"); // for debugging
-   // Serial.println(sensorVal); // for debugging
-    
+      if (now - lastChange >= interval) {  // wait for servo to get there and move to state PAT_UP
+        state = PAT_UP;
+        lastChange = now;
+      }
 
-
+      break;
+    case PAT_UP:
+      patterServo.write(servoUpPos);  // Move servo back to starting position (It resets the servo)
 
 
-    if (now - lastChange >= interval) {  // wait for servo to get there and move to state PAT_UP
-      state = PAT_UP;
-      lastChange = now;
-    }
+      if (now - lastChange >= interval) {  // wait for servo to get there
+        state = IDLE;
+        lastChange = now;
+      }
+      break;
+  }
 
-    break;
-  case PAT_UP:
-    patterServo.write(servoUpPos);  // Move servo back to starting position (It resets the servo)
-    //Serial.println("  PAT_UP"); // for debugging
-    //Serial.println(sensorVal); // for debugging
-    if (now - lastChange >= interval) {  // wait for servo to get there
-      state = IDLE;
-      lastChange = now;
-    }
-    break;
-}
-
-// Debug output
+  // Debug output
   Serial.print("Sensor: ");
   Serial.print(sensorVal);
 
@@ -164,10 +155,5 @@ switch (state) {
     case PAT_UP:
       Serial.println("PAT_UP");
       break;
-
-
-
   }
-
 }
-
